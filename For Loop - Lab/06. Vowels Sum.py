6233dc29f0ce3fd7837f14bd8708ece116ec874d
Dinko Delchev Trendafilov <32400715+DinkoTrendafilov@users.noreply.github.com>
2024-12-02 11:00:20 +0200

    Create 06. Vowels Sum.py

---
+++ For Loop - Lab/06. Vowels Sum.py
@@ -0,0 +1,21 @@
+
+text = input()
+
+counter = 0
+
+for letter in text:
+    if letter == 'a':
+        counter += 1
+    elif letter == 'e':
+        counter += 2
+    elif letter == 'i':
+        counter += 3
+    elif letter == 'o':
+        counter += 4
+    elif letter == 'u':
+        counter +=5
+
+
+
+
+print(counter)
